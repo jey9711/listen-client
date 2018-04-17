d74004f658bd4dbe610c6150c627e4e63ce5bdff
Jeremy Hidajat <jhidajat@uwaterloo.ca>
2018-04-16 21:41:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import Spotify from 'spotify-web-api-js';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import LoginPage from './components/LoginPage';
+import TrackPage from './components/TrackPage';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { 
+      isLoggedIn: true,
+    }
+    this.logIn = this.logIn.bind(this);
+  }
+
+  logIn = () => this.setState({ isLoggedIn: true });
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        {
+          this.state.isLoggedIn 
+            ? <TrackPage />
+            : <LoginPage logIn={this.logIn}/>
+        }
+      </MuiThemeProvider>
+        
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TrackDescription.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import { Grid, Row, Col } from 'react-material-responsive-grid';
+
+class TrackDescription extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <Grid style={{ textAlign: 'center', ...this.props.style }}>
+        <Row>
+          <Col lg={12}>
+            <img
+              src={this.props.albumImgSrc}
+              width="300px"
+              alt=""
+            />
+            <h4 style={{
+              marginTop: '10px',
+              marginBottom: '0px',
+              color: '#ffffff'
+            }}>
+              {this.props.songTitle}
+            </h4>
+            <p style={{
+              fontSize: '13.5px',
+              color: '#777777'
+            }}>
+              {this.props.singer}
+            </p>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+
+export default TrackDescription;
